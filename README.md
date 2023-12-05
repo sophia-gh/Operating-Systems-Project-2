84b6489cf6bfffcf49e379362b5df4ba22a614c8
sophia-gh <stesta4@kent.edu>
2023-12-05 17:32:57 -0500

    Problem With the Algorithm
    does not successfully find safe sequence

--- README.md
+++ README.md
@@ -4,12 +4,16 @@ Repository for the Banker's Algorithm Operating Systems Project
 # Description 
     Language: c++
 
-    This is a solution to the Banker's Algorithm Problem in Operating systems.
+    This is a solution to the Banker's Algorithm Problem in Operating systems. Banker's Algorithm is a solution to deadlock for a systems with multiple processes and multiple instances of multiple resources. Bankers Algoithm fins a safe way to allocate resources within a given system such that deadlock does not occur. This is achived by searching for a sequence of processes where the amount of resources being allocated never exceeds the amount of resources available for all processes in a system. 
 # Compilation Commands
     Compiles using the following commands:
     $ g++ b_algorithm.cpp -pthread -lrt -o b_algorithm
     $ ./b_algorithm
-
+# Commit "Problem Within The Algorithm" 
+    At this point I have implemented the Banker's Algorithm according to the class lecture notes and the Example from Reference 1. There is a problem within my implementation of the algorithm where it loops through the data set to find a sequence that satisifies the following conditions:
+        finish[i] = false;
+        work[i] <= available[i]; 
+    As written currently the code compiles but Prints out "Unsafe!" Despite the data set containing a safe solution. It does not seem to reach line 87 for all "i" in the array correct_sequence. I am not sure why it has encountered this issue. It may be that it is not asssesing the propper values at the propper points within the nested for loops. 
 # References: 
     1: Tutorial Article on GeeksForGeeks Website titled  "Banker's Algorithm in Operating Systems", last
     updated Sept. 2023
--- b_algorithm.cpp
+++ b_algorithm.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(){
     int i = 5;     //hardcoded number of processes, can be easily edited for different data set   
     int j = 3;     //harcoded number of resources
-    int k, o;      //iteration variables
+    int k, o, y, x;      //iteration variables
     
     ifstream indata; //declares ifstream object for the input file
     
@@ -45,5 +45,66 @@ int main(){
 
     //closes file
     indata.close();
+
+    //bankers algorithm 
+    //creates need matrix, need[i][j] = the max of Rj needed by Pi
+    int need[i][j]; 
+    for(k = 0; k < i; k++){
+        for(o = 0; o < j; o++){
+            need[k][o] = maximum[k][o] - allocated[k][o];
+        }
+    }
+
+    //array of booleans that assesses if Pi is finsished being allocated resources
+    bool finish[i]; 
+    for(k = 0; k < i; k++){ finish[k] = false; }   //init to false
+
+
+    //work array that represents the available resources to be worked on 
+    int work[j]; 
+    for(o = 0; o < j; o++){
+        work[o] = available[o];     //initialize with same values as available
+    }
+
+    //find sequence for which each instance of each resource can be worked on and finished according to the
+    //need matrix
+    int correct_sequence[i];                      //array to store correct sequence of processes
+    int pNum = 0;                                 //index for correct sequence array
+    bool satisfies = true; 
+    for(k = 0; k < i; k++){                       // loop through number of processes twice
+        for(o = 0; o < i; o++){                   
+            if(!f[k]){                            //if process k not finished
+                for(y = 0; y < j; y++){           //loop through resources
+                    if (need[o][y] > work[y])    //if the need of process o and resource y 
+                        satisfies = false;        //exceeds the available for resource y 
+                        break;                    //returns out of for loop(line 74)
+                }
+                if (satisfies == true) {
+                    correct_sequence[pNum++] = o; //if process o is worked on and satisfies need < avail it is added to seequence at the correct index
+                    for (x = 0; x < j; x++){    //loop through resources and update availability of work
+                        work[x] = work[x] + allocated[o][x];
+                    }
+                    finish[o] = true;           //process o is finished being worked on 
+                }
+            }
+        }
+    }
+
+    //check if the sequence is safe by checking if each process is finished
+    for(o = 0; o < i; o++){
+        if(!finish[o]){
+            cout << "Unsafe!\n"; 
+            satisfies = false; 
+            break;  
+        }
+    }
+    
+    if(satisfies == true){
+        cout << "Safe! as follows: \n";
+        for (k = 0; k < i - 1; k++)
+            cout << " P" << correct_sequence[k] << " ->";
+            cout << " P" << correct_sequence[k - 1] <<endl;
+    }
+
 }
 //open data file
